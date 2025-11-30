58138753d7c255af664db4cf2dfcd8f30ab42d9a
Sammir Alejandro Bolanos Luna <sammir.bolanos@mvm.com.co>
2025-11-30 18:59:50 -0500

    implementacion hateoas

--- src/main/java/com/citasalud/demo/controller/BloqueoOperativoController.java
+++ src/main/java/com/citasalud/demo/controller/BloqueoOperativoController.java
@@ -2,6 +2,8 @@ package com.citasalud.demo.controller;
 
 import java.util.List;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,6 +18,8 @@ import com.citasalud.demo.models.dtos.BloqueoOperativoDTORequest;
 import com.citasalud.demo.models.dtos.BloqueoOperativoDTOResponse;
 import com.citasalud.demo.service.BloqueoOperativoService;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 @RestController
 @RequestMapping("/api/bloqueos-operativos")
 public class BloqueoOperativoController {
@@ -29,19 +33,38 @@ public class BloqueoOperativoController {
     @PostMapping
     public ResponseEntity<BloqueoOperativoDTOResponse> crear(@RequestBody BloqueoOperativoDTORequest dto) {
         BloqueoOperativoDTOResponse response = service.crear(dto);
+        
+        response.add(linkTo(methodOn(BloqueoOperativoController.class).obtenerPorId(response.getId())).withSelfRel());
+        response.add(linkTo(methodOn(BloqueoOperativoController.class).listar()).withRel("bloqueos"));
+        response.add(linkTo(methodOn(BloqueoOperativoController.class).eliminar(response.getId())).withRel("delete"));
+        
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<BloqueoOperativoDTOResponse> obtenerPorId(@PathVariable Integer id) {
         BloqueoOperativoDTOResponse response = service.obtenerPorId(id);
+        
+        response.add(linkTo(methodOn(BloqueoOperativoController.class).obtenerPorId(id)).withSelfRel());
+        response.add(linkTo(methodOn(BloqueoOperativoController.class).listar()).withRel("bloqueos"));
+        response.add(linkTo(methodOn(BloqueoOperativoController.class).eliminar(id)).withRel("delete"));
+        
         return ResponseEntity.ok(response);
     }
 
     @GetMapping
-    public ResponseEntity<List<BloqueoOperativoDTOResponse>> listar() {
+    public ResponseEntity<CollectionModel<BloqueoOperativoDTOResponse>> listar() {
         List<BloqueoOperativoDTOResponse> lista = service.listar();
-        return ResponseEntity.ok(lista);
+        
+        for (BloqueoOperativoDTOResponse bloqueo : lista) {
+            bloqueo.add(linkTo(methodOn(BloqueoOperativoController.class).obtenerPorId(bloqueo.getId())).withSelfRel());
+            bloqueo.add(linkTo(methodOn(BloqueoOperativoController.class).eliminar(bloqueo.getId())).withRel("delete"));
+        }
+        
+        Link selfLink = linkTo(methodOn(BloqueoOperativoController.class).listar()).withSelfRel();
+        CollectionModel<BloqueoOperativoDTOResponse> collectionModel = CollectionModel.of(lista, selfLink);
+        
+        return ResponseEntity.ok(collectionModel);
     }
 
     @DeleteMapping("/{id}")
